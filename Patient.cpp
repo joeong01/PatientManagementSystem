7c55c48a772161ba1bb8e7eb01f2f977f8243289
Joe Ong <66934953+joeong01@users.noreply.github.com>
2020-11-26 20:49:23 +0800

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -14,30 +14,24 @@ Patient::Patient(int id, int age, string name, string address, string contact, s
 
 //body of getter & setter
 int Patient::getId() { return id; }
-void Patient::setId(int id){ this->id = id;}
-
 int Patient::getAge() { return age; }
-void Patient::setAge(int age) { this->age = age; }
-
 string Patient::getName() { return name; }
-void Patient::setName(string name){ this->name = name; }
-
 string Patient::getAddress() { return address; }
-void Patient::setAddress(string address) { this->address = address; }
-
 string Patient::getContact() { return contact;}
-void Patient::setContact(string contact) { this->contact = contact; }
-
 string Patient::getIllness() { return illness; }
-void Patient::setIllness(string illness) { this->illness = illness; }
-
 string Patient::getDoctorName() { return doctor_name; }
-void Patient::setDoctorName(string doctor_name) { doctor_name = doctor_name; }
-
 bool Patient::getInsurance() { return insurance; }
-void Patient::setInsurance(bool insurance) { this->insurance = insurance; }
-
 string Patient::getMedicine() { return medicine; }
+
+
+void Patient::setId(int id){ this->id = id;}
+void Patient::setAge(int age) { this->age = age; }
+void Patient::setName(string name){ this->name = name; }
+void Patient::setAddress(string address) { this->address = address; }
+void Patient::setContact(string contact) { this->contact = contact; }
+void Patient::setIllness(string illness) { this->illness = illness; }
+void Patient::setDoctorName(string doctor_name) { doctor_name = doctor_name; }
+void Patient::setInsurance(bool insurance) { this->insurance = insurance; }
 void Patient::setMedicine(string medicine) { this->medicine = medicine; }
 
 void Patient::print() {
